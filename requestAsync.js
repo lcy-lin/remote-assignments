bb19c2d4a5c034e08bbc471965d2c69a95c2c104
lcy-lin <linjoy6@gmail.com>
2023-09-30 12:11:36 +0800

    remote-week1: modify console.log in requestAsync and requestSync

--- requestAsync.js
+++ requestAsync.js
@@ -2,22 +2,26 @@ const url = "https://ec2-54-64-246-136.ap-northeast-1.compute.amazonaws.com/dela
 const axios = require('axios');
 
 function requestCallback(url, callback) {
+  const startTime = process.hrtime();
  // write code to request url asynchronously
     axios(url)
     .then(response => {
-            callback(response.data.data);
+            const endTime = process.hrtime(startTime);
+            callback(endTime[0] * 1000 + endTime[1] / 1000000);
         })
     .catch(err => console.error(err))
 }
 function requestPromise(url) {
+  const startTime = process.hrtime();
  // write code to request url asynchronously with Promise
  return new Promise((resolve, reject) => {
     axios.get(url)
       .then(response => {
-        resolve(response.data.data);
+        const endTime = process.hrtime(startTime);
+        resolve(endTime[0] * 1000 + endTime[1] / 1000000);
       })
       .catch(error => {
-        reject(error); // Reject with the error if the request fails
+        reject(error);
       });
   });
 }
@@ -25,8 +29,10 @@ async function requestAsyncAwait(url) {
  // write code to request url asynchronously
  // you should call requestPromise here and get the result using
  // async/await.
+ const startTime = process.hrtime();
   const result = await requestPromise(url);
-  console.log(result);
+  const endTime = process.hrtime(startTime);
+  console.log(endTime[0] * 1000 + endTime[1] / 1000000);
 }
 requestCallback(url, console.log); // would print out the execution time
 requestPromise(url).then(console.log);
--- requestSync.js
+++ requestSync.js
@@ -1,10 +1,11 @@
 const url = "https://ec2-54-64-246-136.ap-northeast-1.compute.amazonaws.com/delay-clock";
 const syncRequest = require('sync-request');
 function requestSync(url) {
+    const startTime = process.hrtime();
     // write code to request url synchronously
     let request = syncRequest('GET', url);
-    let response = JSON.parse(request.getBody());
-    console.log(response.data);
+    const endTime = process.hrtime(startTime);
+    console.log(endTime[0] * 1000 + endTime[1] / 1000000);
 }
 requestSync(url) // would print out the execution time
 requestSync(url)
